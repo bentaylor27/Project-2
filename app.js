9e884e98dcc09c538984df30d5ca9f03083f1226
user <user@users-Air.COMFAST>
2020-11-06 08:57:34 -0600

    app fully functioning

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const { modelName } = require('./models/todo.js')
 const mongoURI = 'mongodb://localhost:27017/' + 'tasks'
 const db = mongoose.connection
 
-
 // Connect to Mongo
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, () => {
   console.log('The connection with mongod is established')
