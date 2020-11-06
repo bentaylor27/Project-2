e00dd774622bf088a81e7e24f2582d3c3803fa11
user <user@users-Air.COMFAST>
2020-11-06 08:44:55 -0600

    show styling done

--- models/todo.js
+++ models/todo.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const toDoSchema = new mongoose.Schema({
     title: {type: String, required: true},
-    createdAt: {type: Date, required: true}, 
+    createdAt: {type: Date, default: Date.now, required: true}, 
     task: {type: String, required: true},
     completed: {type: Boolean, required: true}
 })
